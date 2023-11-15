538f270668e7092bebc1acf75231aa621b9a23fb
Anujha <Anuhja@catina.com>
2023-11-15 18:00:13 +0530

    Table updates

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,64 @@
-import { memo } from "react";
-import { Box } from "@mui/material";
+import React, { memo } from "react";
+import { Box, styled, useTheme } from "@mui/material";
+import { DataGridPro, LicenseInfo } from "@mui/x-data-grid-pro";
 
-const TableComp = () => {
-  return <Box>{/* <Typography>Loading...</Typography> */}</Box>;
+const MUI_DATAGRID_KEY =
+  "2a1203426ca4412164fd0b503afd0b83T1JERVI6MzkyOTgsRVhQSVJZPTE2NzgzODczMjgwMDAsS0VZVkVSU0lPTj0x";
+
+LicenseInfo.setLicenseKey(MUI_DATAGRID_KEY);
+
+const StyledDataGrid = styled(DataGridPro)(() => ({
+  minHeight: "400px",
+  color: "#7E8299",
+  fontSize: 14,
+  fontWeight: 600,
+  border: "none",
+  "&.MuiDataGrid-root .MuiDataGrid-withBorder": {
+    borderRight: "none",
+  },
+  "&.MuiDataGrid-root .MuiDataGrid-columnHeader--moving": {
+    backgroundColor: "transparent",
+  },
+  "& .MuiDataGrid-columnHeaderTitle": {
+    fontSize: 12,
+    color: "#A1A5B7",
+    textAlign: "center",
+    fontWeight: 600,
+    justifyContent: "flex-start",
+  },
+  "& .MuiDataGrid-columnSeparator": {
+    visibility: "hidden",
+  },
+  "& .MuiDataGrid-pinnedColumns": {
+    boxShadow: "none",
+  },
+}));
+
+const TableComp = ({ columns, rows, children, ...rest }) => {
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        height: "100%",
+        width: "100%",
+        borderBottom: 1,
+        borderColor: theme.colors.gray.border,
+        mt: 1,
+        position: "relative",
+      }}
+    >
+      <StyledDataGrid
+        rows={rows}
+        columns={columns}
+        hideFooter={true}
+        showColumnRightBorder
+        rowHeight={100}
+        headerHeight={50}
+        {...rest}
+      />
+      {children}
+    </Box>
+  );
 };
 
 export default memo(TableComp);
--- src/screens/Dashboard/TableContainer.js
+++ src/screens/Dashboard/TableContainer.js
@@ -1,5 +1,5 @@
 import { memo } from "react";
-import { Box, Grid, InputAdornment, Typography } from "@mui/material";
+import { Box, Checkbox, Grid, InputAdornment, Typography } from "@mui/material";
 import { MuiButton, MuiTextInput, TableComp } from "../../components";
 import ProfileIcon from "../../assets/svgIcons/ProfileIcon.svg";
 import arrows from "../../assets/svgIcons/arrows.svg";
@@ -7,22 +7,179 @@ import Icon from "@mdi/react";
 import { mdiChevronDown } from "@mdi/js";
 
 const TableContainer = () => {
-  // const dateList = [
-  //   { value: 1, label: "Today" },
-  //   { value: 2, label: "Yesterday" },
-  //   { value: 3, label: "This Week" },
-  //   { value: 4, label: "This Month" },
-  //   { value: 5, label: "Select Date" },
-  // ];
+  const columns = [
+    {
+      field: "",
+      renderHeader: () => (
+        <Checkbox sx={{ "& .MuiSvgIcon-root": { fontSize: 30 } }} />
+      ),
+      sortable: false,
+      maxWidth: 70,
+      minWidth: 70,
+      disableColumnMenu: true,
+      align: "center",
+      renderCell: () => {
+        return <Checkbox sx={{ "& .MuiSvgIcon-root": { fontSize: 30 } }} />;
+      },
+    },
+    {
+      field: "id",
+      headerName: "ID",
+      sortable: false,
+      maxWidth: 110,
+      minWidth: 110,
+      disableColumnMenu: true,
+      renderCell: (params) => {
+        return <Typography sx={{ color: "#181C32" }}>{params.id}</Typography>;
+      },
+    },
+    {
+      field: "dateRemoved",
+      sortable: false,
+      headerName: "DATE REMOVED",
+      maxWidth: 160,
+      minWidth: 160,
+    },
+    {
+      field: "link",
+      headerName: "LINK",
+      sortable: false,
+      maxWidth: 260,
+      minWidth: 260,
+      renderCell: (params) => {
+        return (
+          <Typography sx={{ color: "#3E97FF", fontSize: 14 }}>
+            {params.row.link}
+          </Typography>
+        );
+      },
+    },
+    {
+      field: "source",
+      headerName: "SOURCE",
+      sortable: false,
+      maxWidth: 240,
+      minWidth: 240,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      sortable: false,
+      width: 100,
+      renderCell: (params) => {
+        return params.row.action === true ? (
+          <Typography
+            style={{
+              fontSize: 12,
+              fontWeight: 600,
+              padding: "7px",
+              color: "#50CD89",
+              borderRadius: "4px",
+              backgroundColor: "#E8FFF3",
+            }}
+          >
+            Removed
+          </Typography>
+        ) : (
+          <Typography
+            style={{
+              fontSize: 12,
+              fontWeight: 600,
+              padding: "7px",
+              color: "#D9214E",
+              borderRadius: "4px",
+              backgroundColor: "#FFF5F8",
+            }}
+          >
+            Delisted
+          </Typography>
+        );
+      },
+    },
+  ];
+
+  const rows = [
+    {
+      id: 148525,
+      dateRemoved: "23 Sep 2023",
+      link: "https://google. com",
+      source: "Google",
+      action: true,
+    },
+    {
+      id: 458722,
+      dateRemoved: "23 Sep 2023",
+      link: "https://google. com",
+      source: "Reddit",
+      action: false,
+    },
+    {
+      id: 158476,
+      dateRemoved: "23 Sep 2023",
+      link: "https://google. com",
+      source: "Twitter",
+      action: true,
+    },
+    {
+      id: 687549,
+      dateRemoved: "23 Sep 2023",
+      link: "https://google. com",
+      source: "Google",
+      action: true,
+    },
+    {
+      id: 845248,
+      dateRemoved: "23 Sep 2023",
+      link: "https://google. com",
+      source: "Reddit",
+      action: true,
+    },
+    {
+      id: 258765,
+      dateRemoved: "23 Sep 2023",
+      link: "https://google. com",
+      source: "Twitter",
+      action: true,
+    },
+    {
+      id: 875492,
+      dateRemoved: "23 Sep 2023",
+      link: "https://google. com",
+      source: "Google",
+      action: false,
+    },
+    {
+      id: 587469,
+      dateRemoved: "23 Sep 2023",
+      link: "https://google. com",
+      source: "Reddit",
+      action: true,
+    },
+    {
+      id: 254817,
+      dateRemoved: "23 Sep 2023",
+      link: "https://google. com",
+      source: "Twitter",
+      action: true,
+    },
+    {
+      id: 125784,
+      dateRemoved: "23 Sep 2023",
+      link: "https://google. com",
+      source: "Google",
+      action: true,
+    },
+  ];
 
   return (
-    <Box>
+    <Box flex={1} style={{ display: "flex", flexDirection: "column" }}>
       <Grid
         style={{
           display: "flex",
           justifyContent: "space-between",
           alignItems: "center",
-        }}>
+        }}
+      >
         <MuiTextInput
           placeholder={"search"}
           sx={{
@@ -47,7 +204,8 @@ const TableContainer = () => {
             justifyContent: "center",
             alignItems: "center",
             gap: "20px",
-          }}>
+          }}
+        >
           <MuiButton
             title={"Export"}
             startIcon={<img src={arrows} alt="img" />}
@@ -68,50 +226,15 @@ const TableContainer = () => {
               gap: "30px",
               backgroundColor: "#F9F9F9",
               padding: "9px 12px",
-            }}>
+            }}
+          >
             <Typography
-              style={{ color: "#A1A5B7", fontSize: 13, fontWeight: 700 }}>
+              style={{ color: "#A1A5B7", fontSize: 13, fontWeight: 700 }}
+            >
               Today
             </Typography>
             <Icon path={mdiChevronDown} color="#A1A5B7" size={1} />
           </Grid>
-          {/* <Grid>
-            <MuiTextInput
-              isSelect={true}
-              placeholder="State"
-              width={112}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="start">
-                    <Icon path={mdiChevronDown} color="#A1A5B7" size={1} />
-                  </InputAdornment>
-                ),
-              }}
-              selectItems={stateDetails}
-              // onChange={(event) => {
-              //   onChangeInput(event.target.value, "state");
-              // }}
-              // error={isError && !edit.allFilled("state")}
-            />
-          </Grid> */}
-          {/* <Grid>
-            <MuiInputSelect
-              isSelect={true}
-              inputLabel="State"
-              required={false}
-              sx={{ width: 112 }}
-              // IconComponent={() => (
-              //   <IconButton>
-              //     <Icon path={mdiChevronDown} size={0.8} />
-              //   </IconButton>
-              // )}
-              selectItems={dateList}
-              // onChange={(event) => {
-              //   onChangeInput(event.target.value, "state");
-              // }}
-              // error={isError && !edit.allFilled("state")}
-            />
-          </Grid> */}
           <Grid
             style={{
               display: "flex",
@@ -120,16 +243,20 @@ const TableContainer = () => {
               gap: "30px",
               backgroundColor: "#F9F9F9",
               padding: "9px 12px",
-            }}>
+            }}
+          >
             <Typography
-              style={{ color: "#A1A5B7", fontSize: 13, fontWeight: 700 }}>
+              style={{ color: "#A1A5B7", fontSize: 13, fontWeight: 700 }}
+            >
               Status
             </Typography>
             <Icon path={mdiChevronDown} color="#A1A5B7" size={1} />
           </Grid>
         </Grid>
       </Grid>
-      <TableComp />
+      <Grid style={{ paddingTop: "30px" }}>
+        <TableComp columns={columns} rows={rows} />
+      </Grid>
     </Box>
   );
 };
